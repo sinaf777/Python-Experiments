929f046eb90bb6396912ad1456ed43c1e7d5bb1b
sinaf777 <sinafdebelireal@gmail.com>
2025-07-05 18:24:23 +0300

    examples

--- stack = [].py
+++ stack = [].py
@@ -1,7 +1,14 @@
-glist = ["nn", "bb", "mm","uu"]
-glist.append("vv")
-glist.insert(1,"cc")
-glist.pop()
-for i  in range(len(glist)-1, -1, -1):
-    print(glist[i], i)
-#list example
+def list_example():
+    glist = ["nn", "bb", "mm","uu"]
+    glist.append("vv")
+    glist.insert(1,"cc")
+    glist.pop()
+    for i  in range(len(glist)-1, -1, -1):
+        print(glist[i], i)
+
+def itration_examle():
+    n = 0
+    for i in range (11):
+        n += i
+    print(n) 
+
