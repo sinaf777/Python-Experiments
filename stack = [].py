bba59bb46301bcef20a7d58a5e7e3591b311fdc1
sinaf777 <sinafdebelireal@gmail.com>
2025-07-05 18:07:38 +0300

    list example

--- stack = [].py
+++ stack = [].py
@@ -1,5 +1,7 @@
-n = 5
-for i in range (n):
-    for j in range (n):
-        print('*', end = ' ')
-    print()
+glist = ["nn", "bb", "mm","uu"]
+glist.append("vv")
+glist.insert(1,"cc")
+glist.pop()
+for i  in range(len(glist)-1, -1, -1):
+    print(glist[i], i)
+#list example
